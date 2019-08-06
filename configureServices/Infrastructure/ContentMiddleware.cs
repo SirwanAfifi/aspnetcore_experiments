dc5a68d5e2e610b77e87dcd8c4a8c80760f165a1
sirwan <sir1afifi@gmail.com>
2019-08-06 10:44:46 +0430

    Using services in custom middleware

--- configureServices/Infrastructure/ContentMiddleware.cs
+++ configureServices/Infrastructure/ContentMiddleware.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using System.Threading.Tasks;
+using configureService.Infrastructure;
 using Microsoft.AspNetCore.Http;
 
 namespace configureServices
@@ -7,14 +8,20 @@ namespace configureServices
     public class ContentMiddleware
     {
         private RequestDelegate nextDelegate;
+        private readonly UptimeService up;
 
-        public ContentMiddleware(RequestDelegate next) => nextDelegate = next;
+        public ContentMiddleware(RequestDelegate next, UptimeService up)
+        {
+            nextDelegate = next;
+            this.up = up;
+        }
 
         public async Task Invoke(HttpContext httpContext)
         {
             if (httpContext.Request.Path.ToString() == "/middleware")
             {
-                await httpContext.Response.WriteAsync("This is from the content middleware", Encoding.UTF8);
+                await httpContext.Response.WriteAsync("This is from the content middleware " +
+                        $"(uptime: {up.Uptime}ms)", Encoding.UTF8);
             }
             else
             {
