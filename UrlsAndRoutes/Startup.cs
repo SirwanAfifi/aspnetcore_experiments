c71bc2d0dffce3932d705d56459accdedd60a7e4
sirwan <sir1afifi@gmail.com>
2019-08-08 10:25:24 +0430

    Use Url.Action

--- UrlsAndRoutes/Startup.cs
+++ UrlsAndRoutes/Startup.cs
@@ -29,6 +29,11 @@ namespace UrlsAndRoutes
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                    name: "out",
+                    template: "outbound/{controller=Home}/{action=Index}"
+                );
             });
         }
     }
