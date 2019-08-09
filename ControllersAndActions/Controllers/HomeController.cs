6b5ceccc5f27ab13bc0261c6a4f26039be539f62
sirwan <sir1afifi@gmail.com>
2019-08-09 11:11:47 +0430

    Generate response

---
+++ ControllersAndActions/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+namespace ControllersAndActions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public void ReceiveForm(string name, string city)
+        {
+            Response.StatusCode = 200;
+            Response.ContentType = "text/html";
+            byte[] content = Encoding.ASCII
+                .GetBytes($"<html><body>{name} lives in {city}</body>");
+            Response.Body.WriteAsync(content, 0, content.Length);
+        }
+    }
+}
