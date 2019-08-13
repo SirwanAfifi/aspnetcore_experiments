7fc3ea22a88eb18227714e0d2c63cbbb69825473
sirwan <sir1afifi@gmail.com>
2019-08-13 18:17:34 +0430

    fixed test

--- DependencyInjection.Tests/DITests.cs
+++ DependencyInjection.Tests/DITests.cs
@@ -15,10 +15,10 @@ namespace DependencyInjection.Tests
             var data = new[] { new Product { Name = "Test", Price = 100 } };
             var mock = new Mock<IRepository>();
             mock.Setup(m => m.Products).Returns(data);
-            HomeController controller = new HomeController(mock.Object);
+            HomeController controller = new HomeController();
 
             // Act
-            ViewResult result = controller.Index();
+            ViewResult result = controller.Index(mock.Object);
 
             // Assert
             Assert.Equal(data, result.ViewData.Model);
