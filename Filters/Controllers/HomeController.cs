cc15dc9a9446fd6016dbdbf906041824a1cec862
sirwan <sir1afifi@gmail.com>
2019-08-15 00:29:06 +0430

    Add RangeException

--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
+using System;
 using Filters.Infrastructure;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Filters.Controllers
 {
-    [Profile]
+    //[Profile]
+    [RangeException]
     public class HomeController : Controller
     {
         public IActionResult Index() => View("Message",
@@ -12,5 +14,21 @@ namespace Filters.Controllers
 
         public IActionResult SecondAction() => View("Message",
                     "This is the SecondAction action on the Home controller");
+
+        public ViewResult GenerateException(int? id)
+        {
+            if (id == null)
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+            else if (id > 10)
+            {
+                throw new ArgumentOutOfRangeException(nameof(id));
+            }
+            else
+            {
+                return View("Message", $"The value is {id}");
+            }
+        }
     }
 }
