6dc2eff4a1273f1821f8c5edf5e234fb340c7669
sirwan <sir1afifi@gmail.com>
2019-08-14 11:17:30 +0430

    Duplication

--- Filters/Controllers/HomeController.cs
+++ Filters/Controllers/HomeController.cs
@@ -1,10 +1,34 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Filters.Controllers
 {
     public class HomeController : Controller
     {
-        public ViewResult Index() => View("Message",
+        public IActionResult Index()
+        {
+            if (!Request.IsHttps)
+            {
+                return new StatusCodeResult(StatusCodes.Status403Forbidden);
+            }
+            else
+            {
+                return View("Message",
             "This is the Index action on the Home controller");
+            }
+        }
+
+        public IActionResult SecondAction()
+        {
+            if (!Request.IsHttps)
+            {
+                return new StatusCodeResult(StatusCodes.Status403Forbidden);
+            }
+            else
+            {
+                return View("Message",
+                    "This is the SecondAction action on the Home controller");
+            }
+        }
     }
 }
