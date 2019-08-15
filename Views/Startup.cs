81ff776c034ea714ccd291d626239f22e1676c97
sirwan <sir1afifi@gmail.com>
2019-08-15 11:54:36 +0430

    Create custom view engine

--- Views/Startup.cs
+++ Views/Startup.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Views.Infrastructure;
 
 namespace Views
 {
@@ -14,6 +16,10 @@ namespace Views
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.Configure<MvcViewOptions>(options =>
+            {
+                options.ViewEngines.Insert(0, new DebugDataViewEngine());
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
