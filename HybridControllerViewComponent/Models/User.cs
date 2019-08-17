b1807c3810d5e1d88b0370592adecf541aedf60f
sirwan <sir1afifi@gmail.com>
2019-08-18 01:48:18 +0430

    Add new project

---
+++ HybridControllerViewComponent/Models/User.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace HybridControllerViewComponent.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+    }
+
+    public static class UserDataSource
+    {
+        public static IList<User> GetUsers()
+        {
+            var list = new List<User>;
+            for (int i = 0; i < 50; i++)
+            {
+                list.Add(new User { Id = i * 2, Name = $"Name {++i}", LastName = $"LastName {++i}", Age = (++i * 2) / 2 });
+            }
+            return list;
+        }
+    }
+}
