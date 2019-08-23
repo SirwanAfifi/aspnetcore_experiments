7831f7a09abae4542f5129de34dd49e76f72268e
sirwan <sir1afifi@gmail.com>
2019-08-24 00:02:25 +0430

    Added Identity MVC App

---
+++ Users/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Users.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index() =>
+            View(new Dictionary<string, object> { ["Placeholder"] = "Placeholder" });
+    }
+}
---
+++ Users/Startup.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Users.Models;
+
+namespace Users
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppIdentityDbContext>(options =>
+            options.UseMySql(Configuration["Data:SportStoreIdentity:ConnectionString"]));
+            services.AddIdentity<AppUser, IdentityRole>()
+                            .AddEntityFrameworkStores<AppIdentityDbContext>()
+                            .AddDefaultTokenProviders();
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseStatusCodePages();
+            app.UseDeveloperExceptionPage();
+            app.UseStaticFiles();
+            app.UseAuthentication();
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
