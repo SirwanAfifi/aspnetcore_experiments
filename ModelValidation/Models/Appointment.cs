47dade029996ac2239a0e6c9314cebfed92722d6
sirwan <sir1afifi@gmail.com>
2019-08-23 10:22:30 +0430

    Added project and model

---
+++ ModelValidation/Models/Appointment.cs
@@ -0,0 +1,13 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidation.Models
+{
+    public class Appointment
+    {
+        public string ClientName { get; set; }
+        [UIHint("Date")]
+        public DateTime Date { get; set; }
+        public bool TermsAccepted { get; set; }
+    }
+}
