88e6e97337f68d4a8fe2931ebc77e30ed1a757e4
sirwan <sir1afifi@gmail.com>
2019-08-23 10:36:01 +0430

    Add custom validator

--- ModelValidation/Models/Appointment.cs
+++ ModelValidation/Models/Appointment.cs
@@ -1,5 +1,6 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using ModelValidation.Infrastructure;
 
 namespace ModelValidation.Models
 {
@@ -8,6 +9,7 @@ namespace ModelValidation.Models
         public string ClientName { get; set; }
         [UIHint("Date")]
         public DateTime Date { get; set; }
+        [MustBeTrue(ErrorMessage = "You must accept the terms")]
         public bool TermsAccepted { get; set; }
     }
 }
