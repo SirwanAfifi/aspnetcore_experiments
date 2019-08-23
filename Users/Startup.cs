9693cf93ce7c5cdf79783918f38f2b810137f698
sirwan <sir1afifi@gmail.com>
2019-08-24 00:09:30 +0430

    Customize validation

--- Users/Startup.cs
+++ Users/Startup.cs
@@ -18,9 +18,17 @@ namespace Users
         {
             services.AddDbContext<AppIdentityDbContext>(options =>
             options.UseMySql(Configuration["Data:SportStoreIdentity:ConnectionString"]));
-            services.AddIdentity<AppUser, IdentityRole>()
-                            .AddEntityFrameworkStores<AppIdentityDbContext>()
-                            .AddDefaultTokenProviders();
+            services.AddIdentity<AppUser, IdentityRole>(opts =>
+            {
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+
 
             services.AddMvc();
         }
