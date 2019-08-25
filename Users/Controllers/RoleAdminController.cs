a5644437245a9af7610524f1b46cd1c8ed55c52f
sirwan <sir1afifi@gmail.com>
2019-08-25 17:10:41 +0430

    Add Edit Role

--- Users/Controllers/RoleAdminController.cs
+++ Users/Controllers/RoleAdminController.cs
@@ -1,17 +1,22 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Users.Models;
 
 namespace Users.Controllers
 {
     public class RoleAdminController : Controller
     {
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly UserManager<AppUser> userMrg;
 
-        public RoleAdminController(RoleManager<IdentityRole> roleMgr)
+        public RoleAdminController(RoleManager<IdentityRole> roleMgr,
+        UserManager<AppUser> userMrg)
         {
             roleManager = roleMgr;
+            this.userMrg = userMrg;
         }
 
         public ViewResult Index() => View(roleManager.Roles);
@@ -61,6 +66,68 @@ namespace Users.Controllers
             foreach (IdentityError error in result.Errors)
             {
                 ModelState.AddModelError("", error.Description);
+            }
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            List<AppUser> members = new List<AppUser>();
+            List<AppUser> nonMembers = new List<AppUser>();
+            foreach (AppUser user in userMrg.Users)
+            {
+                var list = await userMrg.IsInRoleAsync(user, role.Name)
+                    ? members : nonMembers;
+                list.Add(user);
+            }
+            return View(new RoleEditModel
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(RoleModificationModel model)
+        {
+            IdentityResult result;
+            if (ModelState.IsValid)
+            {
+                foreach (string userId in model.IdsToAdd ?? new string[] { })
+                {
+                    AppUser user = await userMrg.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        result = await userMrg.AddToRoleAsync(user,
+                            model.RoleName);
+                        if (!result.Succeeded)
+                        {
+                            AddErrorsFromResult(result);
+                        }
+                    }
+                }
+                foreach (string userId in model.IdsToDelete ?? new string[] { })
+                {
+                    AppUser user = await userMrg.FindByIdAsync(userId);
+                    if (user != null)
+                    {
+                        result = await userMrg.RemoveFromRoleAsync(user,
+                            model.RoleName);
+                        if (!result.Succeeded)
+                        {
+                            AddErrorsFromResult(result);
+                        }
+                    }
+                }
+            }
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return await Edit(model.RoleId);
             }
         }
     }
