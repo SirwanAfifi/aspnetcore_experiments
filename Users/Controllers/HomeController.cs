d5753d74b63ef7a35151003140c79540b75e4d11
sirwan <sir1afifi@gmail.com>
2019-08-25 17:18:29 +0430

    Add new fields to AppUser

--- Users/Controllers/HomeController.cs
+++ Users/Controllers/HomeController.cs
@@ -1,12 +1,22 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Users.Models;
 
 namespace Users.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private UserManager<AppUser> userManager;
+        public HomeController(UserManager<AppUser> userMgr)
+        {
+            userManager = userMgr;
+        }
+
         [Authorize]
         public IActionResult Index() => View(GetData(nameof(Index)));
         [Authorize(Roles = "Users")]
@@ -21,7 +31,33 @@ namespace Users.Controllers
                 ["User"] = HttpContext.User.Identity.Name,
                 ["Authenticated"] = HttpContext.User.Identity.IsAuthenticated,
                 ["Auth Type"] = HttpContext.User.Identity.AuthenticationType,
-                ["In Users Role"] = HttpContext.User.IsInRole("Users")
+                ["In Users Role"] = HttpContext.User.IsInRole("Users"),
+                ["City"] = CurrentUser.Result.City,
+                ["Qualification"] = CurrentUser.Result.Qualifications
             };
+
+        [Authorize]
+        public async Task<IActionResult> UserProps()
+        {
+            return View(await CurrentUser);
+        }
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> UserProps(
+                [Required]Cities city,
+                [Required]QualificationLevels qualifications)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await CurrentUser;
+                user.City = city;
+                user.Qualifications = qualifications;
+                await userManager.UpdateAsync(user);
+                return RedirectToAction("Index");
+            }
+            return View(await CurrentUser);
+        }
+        private Task<AppUser> CurrentUser =>
+            userManager.FindByNameAsync(HttpContext.User.Identity.Name);
     }
 }
