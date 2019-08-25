5348dedc0784eb587afbcb81621626e93da867ce
sirwan <sir1afifi@gmail.com>
2019-08-25 16:51:06 +0430

    Add Login

--- Users/Controllers/HomeController.cs
+++ Users/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Users.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ViewResult Index() =>
