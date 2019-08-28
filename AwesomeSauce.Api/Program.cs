4c88c19a1227b8a10e06cc07eb59a95ab215556c
sirwan <sir1afifi@gmail.com>
2019-08-28 11:53:11 +0430

    Override built-in server

--- AwesomeSauce.Api/Program.cs
+++ AwesomeSauce.Api/Program.cs
@@ -20,5 +20,6 @@ namespace AwesomeSauce.Api
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+        // .UseHttpSys(); // Windows-only server
     }
 }
