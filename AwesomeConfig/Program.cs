09639677a810994d01a09242d3ce9b0389e610e7
sirwan <sir1afifi@gmail.com>
2019-08-30 10:57:24 +0430

    Read from config

---
+++ AwesomeConfig/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace AwesomeConfig
+{
+    class Program
+    {
+        public static IConfigurationRoot Configuration { get; set; }
+        static void Main(string[] args)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("awesomeConfig.json");
+            Configuration = builder.Build();
+
+            foreach (var item in Configuration.AsEnumerable())
+            {
+                Console.WriteLine($"Key: {item.Key}, Value: {item.Value}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
